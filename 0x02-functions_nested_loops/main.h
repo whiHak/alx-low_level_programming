6271a3f72ab6fb58823a04b8aaf4b97fb69f464e
whiHak <betse1111111111@gmail.com>
2023-08-16 13:09:32 -0700

    files added

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,14 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_10times(void);
-
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
